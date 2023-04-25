5a2d9f3f00447c502ceae34394c6b16a3762d615
abdimk <abdisamk@gmail.com>
2023-04-25 16:45:45 +0300

    frame has been limited to 60fps

--- src/Game.cpp
+++ src/Game.cpp
@@ -4,7 +4,7 @@
 
 Game::Game() : m_context(std::make_shared<Context>())
 {
-    m_context->m_window->create(sf::VideoMode(400, 400), "Snake!", sf::Style::Close);
+    m_context->m_window->create(sf::VideoMode(400, 400), "Snake!",sf::Style::Close);
 
 
 }
@@ -15,21 +15,40 @@ Game::~Game()
 
 void Game::Run()
 {
-    sf::CircleShape shape(100.f);
+    sf::CircleShape shape(200.f);
     shape.setFillColor(sf::Color::Green);
 
+    sf::Clock clock;
+    sf::Time timeSinceLastFrame = sf::Time::Zero;
     while (m_context->m_window->isOpen())
     {
-        sf::Event event;
-        while (m_context->m_window->pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                m_context->m_window->close();
-        }
+        timeSinceLastFrame += clock.restart();
 
-        m_context->m_window->clear();
-        m_context->m_window->draw(shape);
-        m_context->m_window->display();
+        while(timeSinceLastFrame > TIME_PER_FRAME)
+        {
+            timeSinceLastFrame -= TIME_PER_FRAME;
+            //
+            // m_context-> m_states->ProcessStateChange();
+            // m_context->m_states->GetCurrent()->ProcessInput();
+            // m_context->m_states->GetCurrent()->Update(TIME_PER_FRAME);
+            // m_context->m_states->GetCurrent()->Draw();
+
+
+
+
+
+            sf::Event event;
+            while (m_context->m_window->pollEvent(event))
+            {
+                if (event.type == sf::Event::Closed)
+                    m_context->m_window->close();
+            }
+
+            m_context->m_window->clear();
+            m_context->m_window->draw(shape);
+            m_context->m_window->display();
+        }
+       
     }
 
 
