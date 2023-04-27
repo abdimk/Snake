8073703c70d03e57e1c7e558dd69e0a2d72441c3
abdimk <abdisamk@gmail.com>
2023-04-27 21:27:41 +0300

    commit

--- src/GamePlay.cpp
+++ src/GamePlay.cpp
@@ -17,14 +17,23 @@ void GamePlay:: Init()
 
         // loading the textures in to the sprite 
         m_context->m_assets->AddTexture(GRASS, "utils/textures/grass.png", true);
-        m_context->m_assets->AddTexture(FOOD, "utils/textures/grass.png", false);
-        m_context->m_assets->AddTexture(WALL, "utils/textures/grass.png", true);
-        m_context->m_assets->AddTexture(SNAKE, "utils/textures/grass.png", false);
+        m_context->m_assets->AddTexture(FOOD, "utils/textures/food.png", false);
+        m_context->m_assets->AddTexture(WALL, "utils/textures/wall.png", true);
+        m_context->m_assets->AddTexture(SNAKE, "utils/textures/snake.png", false);
         
         //setting them on the sprite
         m_grass.setTexture(m_context->m_assets->GetTexture(GRASS));
         m_grass.setTextureRect(m_context->m_window->getViewport(m_context->m_window->getDefaultView()));
         
+
+        for(auto& wall : m_walls)
+        {
+                wall.setTexture(m_context->m_assets->GetTexture(WALL));
+        }
+
+
+        //set baundary for the walls
+        m_walls[0].setTextureRect({0, 0, 640, 16});
 }
 void GamePlay:: ProcessInput()
 {
@@ -45,6 +54,13 @@ void GamePlay:: Draw()
 {
         m_context->m_window->clear();
         m_context->m_window->draw(m_grass);
+
+
+        // darw the four walls(each wall on the render window)
+        for(auto& wall : m_walls)
+        {
+                m_context->m_window->draw(wall);
+        }
         m_context->m_window->display();
 }
 
